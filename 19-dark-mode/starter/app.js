47a96c146e2e60f7fedbbf026d8440d9c7a64444
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-03-08 01:08:05 +0530

    19 dark mode done

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,28 @@
-console.log('dark mode');
+const btn = document.querySelector(".btn");
+const articlesElement = document.querySelector(".articles");
+
+btn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+const articleEle = articles
+  .map((article) => {
+    const { id, title, date, length, snippet } = article;
+
+    let d = moment(date).format("MMMM Do YYYY");
+
+    const art = ` <article class="post">
+          <h1 class="title">${title}</h1>
+          <div class="info" data-id="1">
+            <span class="date">${d}</span>
+            <span class="read-time">${length} min read</span>
+          </div>
+          <div class="content">
+           ${snippet}
+          </div>
+        </article>`;
+    return art;
+  })
+  .join("");
+
+articlesElement.innerHTML = articleEle;
