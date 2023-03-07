0a3a99786cfb0c9acd5e76f52791dcbc0f36c71b
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-03-08 00:06:32 +0530

    18 numbers done

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,24 @@
-console.log('numbers project');
+let items = document.querySelectorAll(".value");
+
+const updateCount = (el) => {
+  let id = parseInt(el.dataset.id);
+
+  let increment = Math.ceil(id / 1000);
+
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > id) {
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = `${initialValue}+`;
+  }, 1);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
