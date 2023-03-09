ebba4578fca1ceafe6474ab61bd36acaa937160f
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-03-09 14:33:53 +0530

    21 dad jokes done

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -1,1 +1,31 @@
-console.log('Dad Jokes Starter');
+const btn = document.querySelector(".btn");
+
+const result = document.querySelector(".result");
+
+const url = "https://icanhazdadjoke.com/";
+
+btn.addEventListener("click", async () => {
+  result.textContent = "Loading....";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "user-Agent": "learning app",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("some error occured");
+    }
+
+    const data = await response.json();
+    display(data.joke);
+  } catch (err) {
+    console.log(err);
+    result.textContent = "Some error occured";
+  }
+});
+
+function display(joke) {
+  result.textContent = joke;
+}
