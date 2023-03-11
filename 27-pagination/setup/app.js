54fb453f873e919dc7fd4e5f4f1bf1abc6d5026c
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-03-11 17:37:03 +0530

    27 pagination done

--- 27-pagination/setup/app.js
+++ 27-pagination/setup/app.js
@@ -1,4 +1,45 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const btnContainer = document.querySelector(".btns-container");
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(pages, index);
+};
+
+const start = async () => {
+  const data = await fetchFollowers();
+  pages = paginate(data);
+  setupUI();
+};
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.textContent) - 1;
+  }
+
+  if (e.target.classList.contains("prev")) {
+    console.log("prev btn pressed");
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+
+  if (e.target.classList.contains("next")) {
+    console.log("next button pressed");
+    index++;
+    if (index > pages.length) {
+      index = 0;
+    }
+  }
+  setupUI();
+});
+
+document.addEventListener("DOMContentLoaded", start);
--- 27-pagination/setup/paginate.js
+++ 27-pagination/setup/paginate.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (data) => {
+  const followersperPage = 10;
+  const noOfPages = Math.ceil(data.length / followersperPage);
+  const pages = Array.from({ length: noOfPages }, (_, index) => {
+    let start = index * followersperPage;
+    return data.slice(start, start + followersperPage);
+  });
+  return pages;
+};
 
-export default paginate
+export default paginate;
