ae17facf534022d5be8e97acc7ff33624bc3df63
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-03-21 23:59:10 +0530

    29 comfy store layout finished

--- 29-comfy-store/starter/src/pages/products.js
+++ 29-comfy-store/starter/src/pages/products.js
@@ -1,14 +1,21 @@
 // global imports
-import '../toggleSidebar.js';
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 
 //  filter imports
-import setupSearch from '../filters/search.js';
-import setupCompanies from '../filters/companies.js';
-import setupPrice from '../filters/price.js';
+import setupSearch from "../filters/search.js";
+import setupCompanies from "../filters/companies.js";
+import setupPrice from "../filters/price.js";
 
 // specific imports
-import { store } from '../store.js';
-import display from '../displayProducts.js';
-import { getElement } from '../utils.js';
+import { store } from "../store.js";
+import display from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+const sliderValue = getElement(".slider-value");
+const slider = getElement(".slider");
+
+slider.oninput = function () {
+  sliderValue.innerHTML = `Value : ${slider.value}`;
+};
